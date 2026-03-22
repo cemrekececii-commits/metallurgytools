29d2d999e4c958c5a00f717a549e1b45d70eab46
Cemre Kececi <cemrekececii@gmail.com>
2026-03-22 22:19:20 +0300

    middleware fix v2

--- middleware.js
+++ middleware.js
@@ -1,4 +1,6 @@
-export { default } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
